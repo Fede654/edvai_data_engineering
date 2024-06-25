a862417e74efdb2c5fd19e3df8e7888733a86174
Fede654 <buzondefede@gmail.com>
2024-06-25 04:42:20 -0300

    Trabajo Final.

--- 00-Trabajo_Final/querys_rental.sql
+++ 00-Trabajo_Final/querys_rental.sql
@@ -1,10 +1,16 @@
 -- Ejercicio 5.a
-SELECT COUNT(*) from car_rental_analytics cra
+SELECT COUNT(*) 
+FROM car_rental_analytics cra
 WHERE fueltype != 'gasoline'
 	and rating >= 4
 ;
 -- Ejercicio 5.b
-
+SELECT COUNT (*) as total_alquileres, cra.name_state 
+FROM car_rental_analytics cra
+GROUP BY cra.name_state
+ORDER BY total_alquileres asc
+LIMIT 5
+;
 -- Ejercicio 5.c
 
 -- Ejercicio 5.d
